fbc9fc919a19d8ebf1c219c719511a5a501e5625
Matt Van <mattvanveenendaal@gmail.com>
2024-10-20 13:35:55 +0000

    docs

--- src/poll.rs
+++ src/poll.rs
@@ -5,29 +5,47 @@ use std::{
 }
 use crate::ffi;
 
+/**
+ * Represents Event Queue
+ */
 pub struct Poll {
     registry: Registry,
 }
 
 impl Poll {
+    /**
+     * Create a new event queue
+     */
     pub fn new() -> Result<Self> {
         todo!()
     }
 
+    /**
+     * Returns a reference to the registry we can use to register interest aboutb new events
+     */
     pub fn registry(&self) -> &Registry {
         &self.registry
     }
 
+    /**
+     * Blocks the thread it's called on until an event is ready or it times out
+     */
     pub fn poll(&mut self, events: &mut Events, timout: Option<i32>) -> Result<()> {
         todo!()
     }
 }
 
+/**
+ * Allows us to register new events on the system
+ */
 pub struct Registry {
     raw_fd: i32,
 }
 
 impl Registry {
+    /**
+     * Mimic's mio's register schema.
+     */
     pub fn register(&self, source: &TcpStream, token: usize, interests: i32) -> Result<()> 
     {
         todo!()
